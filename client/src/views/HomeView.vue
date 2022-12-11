5f5dcf695af08754c2b3d6120b050fbf95d2adbc
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2022-12-10 21:18:18 -0500

    Server Setup
    - Setup Backend Server
    - Frontend Started

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import leaflet from 'leaflet';
+import { onMounted } from 'vue';
+
+let map;
+onMounted(() => {
+    // init map
+    map = leaflet.map('map').setView([51.505, -0.09], 13);
+  })
+</script>
+
+<template>
+  <div class="h-screen relative">
+    <div id="map" class="h-full z-[1]"></div>
+  </div>
+</template>
