8197299a207d85752586e4829a2ac1913b81eba4
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2022-12-16 17:49:23 -0500

    Map Tiles and Location
    - Added map tiles to frontend
    - Added Marker
    - Added Current Location

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,12 +1,72 @@
 <script setup>
 import leaflet from 'leaflet';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 
 let map;
 onMounted(() => {
     // init map
-    map = leaflet.map('map').setView([51.505, -0.09], 13);
+    map = leaflet.map('map').setView([30.332, -81.655], 11);
+
+     // add tile layer 
+  leaflet
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+getGeolocation()
   })
+
+  const coords = ref(null);
+  const fetchCoords = ref(null);
+  const geoMarker = ref(null)
+
+  const getGeolocation = () => {
+    fetchCoords.value = true
+    navigator.geolocation.getCurrentPosition(setCoords, getLocError)
+  };
+
+  const setCoords = (pos) => {
+    // stop fetching coords
+    fetchCoords.value = null;
+
+    // set coords in session storage
+    const setSessionCoords = {
+        lat: pos.coords.latitude,
+        lng: pos.coords.longitude,
+      };
+      sessionStorage.setItem("coords", JSON.stringify(setSessionCoords));
+
+    // set ref coords value
+    coords.value = setSessionCoords;
+
+    plotGeolocation(coords.value);
+  };
+
+  const getLocError = (err) => {
+    console.log(err)
+
+  };
+
+  const plotGeolocation = (coords) => {
+    // create custom marker
+    const customMarker = leaflet.icon({
+      iconUrl:('../assets/map-marker-red.png'),
+      iconSize: [35,35]
+    });
+
+    // create new marker with coords and custom icon 
+
+    geoMarker.value = leaflet.marker([coords.lat, coords.lng]).addTo(map);
+
+    // set map view to current location
+    map.setView(coords.lat, coords.lng)
+
+    return { coords, geoMarker };
+  };
+
+  
+   
 </script>
 
 <template>
